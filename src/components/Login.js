40f95b4844fa96a4d7f534120469d6c890abd1ae
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-15 10:24:39 +0530

    react events and more

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Login = () => {
 
-    const [loginData, setLoginData] = useState();
+    const [loginData, setLoginData] = useState({ username: '', password: '' });
     const [loginMessage, setLoginMessage] = useState();
 
     useEffect(() => {
@@ -12,20 +12,34 @@ const Login = () => {
         setLoginMessage('');
     }, []);
 
-    const submitLogin = () => { };
+    const submitLogin = () => {
+        console.log('login successful!');
+    };
+
+    const handleUsername = (evt) => {
+        console.log(evt.target.name);
+        console.log(evt.target.value);
+        setLoginData({ username: evt.target.value });
+    };
+
+    const handlePassword = (evt) => {
+        console.log(evt.target.name);
+        console.log(evt.target.value);
+        setLoginData({ password: evt.target.value });
+    };
 
     return (
         <>
             <h1>Login Component</h1>
             <form onSubmit={submitLogin}>
-                <input type="text" name="username" value={''} onChange={''} />
+                <input type="text" name="username" value={loginData.username} onChange={handleUsername} />
                 <br />
-                <input type="password" name="password" value={''} onChange={''} />
+                <input type="password" name="password" value={loginData.password} onChange={handlePassword} />
                 <br />
                 <input type="submit" value="Login" />
             </form>
             {loginMessage &&
-                <p>{loginMessage}</p>
+                <p>Message:{loginMessage}</p>
             }
         </>
     );
