3408b8e4b7c9669807574b4e6db81c96bd26b590
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-18 14:54:09 +0530

    react routes and other stuff

--- src/components/Life.js
+++ src/components/Life.js
@@ -1,48 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
 // Phases in react component's lifecycle 
 // mounting 
 // updating 
 // unmounting  
 
-class Life extends Component {
+const Life = () => {
 
-    constructor(props) {
-        super(props);
-        console.log(this.props);
-        // equivalent to useState() V 
-        this.state = { salary: 10 };
-    }
+    // equivalent to useState()  in class component 
+    const [salary, setSalary] = useState(10);
 
-    increaseSalary = () => {
-        this.setState((prevState) => ({
-            salary: prevState.salary + 10
-        }));
+    useEffect(() => {
+        console.log('useEffect');
+    });
+
+    const increaseSalary = () => {
+        setSalary(salary + 10);
     };
 
-    componentDidMount() {
-        console.log('componentDidMount');
-    };
-
-    componentDidUpdate() {
-        console.log('componentDidUpdate');
-    }
-
-    componentWillUnmount() {
-        console.log('componentWillUnmount');
-    }
-
-    render() {
-        console.log('render()');
-        return (
-            <div>
-                <p>Lifecycle Component</p>
-                <p>Salary: {this.state.salary} </p>
-                <button onClick={this.increaseSalary} >Click me</button>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <p>Lifecycle Component</p>
+            <p>Salary: {salary} </p>
+            <button onClick={increaseSalary} >Click me</button>
+        </div>
+    );
 }
 
 export default Life;
+
+
+// import React, { Component } from 'react';
+
+// // Phases in react component's lifecycle
+// // mounting
+// // updating
+// // unmounting
+
+// class Life extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         console.log(this.props);
+//         // equivalent to useState() in function component
+//         this.state = { salary: 10 };
+//     }
+
+//     increaseSalary = () => {
+//         this.setState((prevState) => ({
+//             salary: prevState.salary + 10
+//         }));
+//     };
+
+//     componentDidMount() {
+//         console.log('componentDidMount');
+//     };
+
+//     componentDidUpdate() {
+//         console.log('componentDidUpdate');
+//     }
+
+//     componentWillUnmount() {
+//         console.log('componentWillUnmount');
+//     }
+
+//     render() {
+//         console.log('render()');
+//         return (
+//             <div>
+//                 <p>Lifecycle Component</p>
+//                 <p>Salary: {this.state.salary} </p>
+//                 <button onClick={this.increaseSalary} >Click me</button>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Life;
 
