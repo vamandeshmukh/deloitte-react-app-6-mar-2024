3451f32a5ed9760a78ce2a051917fb44d25998ce
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-18 17:59:48 +0530

    react routes and other stuff

--- src/components/context/ThemedComponent.js
+++ src/components/context/ThemedComponent.js
@@ -1,15 +1,12 @@
-import { useTheme } from "./ThemeContext";
 
+import React from 'react';
+import { useTheme } from './ThemeContext';
 
 const ThemedComponent = () => {
-
     const { theme } = useTheme();
 
     return (
-        // <div style={{ color: theme }}>
-        <div style={{ color: theme === 'light' ? 'dark' : 'white', background: 'white' }}>
-        </div>
-
+        <div style={{ color: theme === 'light' ? 'black' : 'white', background: theme === 'light' ? 'white' : 'black', padding: '20px' }}>Themed Component</div>
     );
 };
 
