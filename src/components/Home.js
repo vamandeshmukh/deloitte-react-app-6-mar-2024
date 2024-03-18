a0f0be8550532a546a327e65bdb83a2bb4ec2b6f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-18 16:45:53 +0530

    react routes and other stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,12 @@ const Home = () => {
 
     return (
         <>
-            <div style={{ minHeight: "100vh" }}>
-                <div style={{ minHeight: "100vh" }} className="row">
-                    <div className="col">
-                        <img width="100%" src="https://clipart-library.com/images_k/shopping-transparent-background/shopping-transparent-background-12.png" alt="shopping" />
-                    </div>
-                    <div className="col">
-                        <Link className="lead text-primary" style={{ textDecoration: 'none' }} to='/product-list'>Start shopping...</Link>
-                    </div>
+            <div className="row">
+                <div className="col">
+                    <img width="100%" src="https://clipart-library.com/images_k/shopping-transparent-background/shopping-transparent-background-12.png" alt="shopping" />
+                </div>
+                <div className="col">
+                    <Link to="/product-list" className="lead text-primary link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Start shopping...</Link>
                 </div>
             </div>
         </ >
