36f326a6dad25858ccaa2d203ed52d4d343339af
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-19 16:19:39 +0530

    flux redux and more

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+
+// steps to use redux 
+// 0. install redux libraries 
+// ,
+//     "@reduxjs/toolkit": "2.2.1",
+//     "react-redux": "9.1.0"
+// 1. Create store 
+// 2. Provide store to React app - index.js
+// 3. create reducers / slices 
+// 4. send data to store 
+// 5. Wherever needed, access data from the store 
+
+const store = '';
+
+export default store;
+
